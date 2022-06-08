449a0424d632191525fb0942303d37f1963bcd96
Akiba <akegarasu.akiba@gmail.com>
2022-06-08 22:05:36 +0800

    设置示例

---
+++ config/constants.example.js
@@ -0,0 +1,35 @@
+let config = {
+    Name: "",
+    
+    BiliLiveRoomID: "",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2022 秋葉",
+
+    LanguageCategories: ["日语", "英语", "粤语"],
+    RemarkCategories: ["流行", "弹唱"],
+
+    BannerTitle: "",
+    BannerContent: [
+        `秋葉喵`,
+     ],
+
+    CustomButtons: [
+        {
+            link: "https://www.tapechat.net/",
+            name: "提问箱",
+            image: "/assets/icon/tapechat.png"
+        },        
+        {
+            link: "https://space.bilibili.com/",
+            name: "录播组",
+            image: ""
+        }
+    ]
+}
+
+
+module.exports = {
+    config
+}
